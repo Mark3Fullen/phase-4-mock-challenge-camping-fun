38843f8fac901d93aeed7a6019de2a4c3c0245c2
Mark <mark3fullen@gmail.com>
2021-09-03 10:39:20 -0400

    commit tree 4b6a37f847c2604cf730d16503400932826eaa5e

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,19 @@
+class ActivitiesController < ApplicationController
+
+    def index
+        activities = Activity.all
+        render json: activities, status: :ok
+    end
+
+    def destroy
+        activity = Activity.find_by(id: params[:id])
+        if activity
+            activity.signups.destroy_all
+            activity.destroy
+            head :no_content, status: :no_content
+        else
+            render json: { "error": "Activity not found" }, status: :not_found
+        end
+    end
+
+end
